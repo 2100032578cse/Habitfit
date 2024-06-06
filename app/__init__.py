ef926b09267d03b9ebac3e1c3475e1f04ca4928b
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-06-06 15:36:06 +0530

    soved conflict in requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
+
+# from flask_bootstrap import Bootstrap
 from config import config
 
-bootstrap = Bootstrap()
+# bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
@@ -13,7 +14,8 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
-    bootstrap.init_app(app)
+    # bootstrap.init_app(app)
+    login_manager.init_app(app)
 
     # Register blueprints
     from .main.routes import main as main_blueprint
